b1fcbff1af20aba061f93f4d0538f15869e6893f
Yuri Rodrigues <ysrodrigues@gmail.com>
2021-06-15 11:32:34 -0300

    add eslint, prettier configured for vscode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title: String = 'piratasdeb';
+  title: String = 'piratasdeb'
 
   public clickMe(): void {
-    console.log('heelo');
-    return;
+    console.log('heelo')
+    return
   }
 }

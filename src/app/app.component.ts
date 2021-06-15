b4a3386f24f0bdb7a61340a499710d1967ee28d9
Yuri Rodrigues <ysrodrigues@gmail.com>
2021-06-15 10:12:32 -0300

    header and footer done and installed tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'piratasdeb';
+  title: String = 'piratasdeb';
+
+  public clickMe(): void {
+    console.log('heelo');
+    return;
+  }
 }

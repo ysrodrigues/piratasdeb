6fa0da546e51278091feca4d62fd929156649390
Yuri Rodrigues <ysrodrigues@gmail.com>
2021-06-15 12:02:29 -0300

    fix eslint config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,4 +10,10 @@ import { AppComponent } from './app.component'
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  title = 'hello'
+
+  public click() {
+    return 'heelo'
+  }
+}

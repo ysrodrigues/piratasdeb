c0078448d60987e132202f6c9ef8a42de1e1e80f
Yuri Rodrigues <ysrodrigues@gmail.com>
2021-06-29 12:18:31 -0300

    add gts lint and init <main>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title: String = 'piratasdeb'
-
-  public clickMe(): void {
-    console.log('heelo')
-    return
-  }
+  title = 'piratasdeb'
 }
